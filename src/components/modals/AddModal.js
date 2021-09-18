9e0505e123344fcf248ee8942f9ce17e74cbd274
Ganesh Srambikal <ganesh.srambikal@gmail.com>
2021-09-19 00:42:12 +0530

    addes ss

--- src/components/modals/AddModal.js
+++ src/components/modals/AddModal.js
@@ -140,6 +140,7 @@ const AddModal = ({fetchAllData}) => {
           </div>
           <div className="modal-footer">
             <button type="button" className="btn btn-secondary" data-bs-dismiss="modal" onClick={clearFields}>Close</button>          
+            
           </div>
         </div>
        </div>
